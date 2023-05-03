5e20f3fe4237641481ee5858ca63f8cf353332c3
Andoni <juakata07@gmail.com>
2023-05-02 20:27:00 -0600

    :hammer: add service cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Toyota', 'Jeep', 'Nisan', 'Honda'];
-
   @Get()
   getAllCars() {
     return this.cars;
