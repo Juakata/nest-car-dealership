e8bc138f6541d1ba6c851d61ae9e1a5fdca6491d
Andoni <juakata07@gmail.com>
2023-05-02 20:10:35 -0600

    :hammer: add get and getAll requests for cars

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  private cars = ['Toyota', 'Jeep', 'Nisan', 'Honda'];
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: number) {
+    return this.cars[id];
+  }
+}
